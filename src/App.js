e6973431c0a27f50c48b487fc0e3d9a8110a9e46
nicolasfrias-UDP <68442990+nicolasfrias-UDP@users.noreply.github.com>
2023-11-03 13:31:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
+function App() { 
   return <div>
     <h1> Welcome to computer vision app </h1>
     <p>Insert Url to ananlize image or promp to generate an image</p>
     <input type="text" placeholder="Url" />
-    <buttion>ananlize</buttion><button>generate</button>
+    <button>ananlize</button><button>generate</button>
   </div>;
 }
 
